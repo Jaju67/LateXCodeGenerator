bd67a975b332912884ccad0ecd36cf9b0fb38d81
jasprogramista <jan.wie.niewiele@gmail.com>
2023-09-09 00:28:39 +0200

    HAHA DZIAŁA, dodatkowo wyświetlanie png

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
- 
+import tkinter as tk
+from PIL import Image, ImageDraw
+import PIL
+
+import interface
+import interpreterapi
+
+
+window = tk.Tk()
+window.geometry('600x400')
+window.title('wpisz równanie')
+
+canvas = tk.Canvas(window, width=600, height=400, bg = 'grey')
+canvas.pack()
+
+image = PIL.Image.new('RGB', (600, 400), (255, 255, 255))
+
+interface.rysowanie_hud()
+
+interface.draw(canvas, image)
+window.mainloop()
