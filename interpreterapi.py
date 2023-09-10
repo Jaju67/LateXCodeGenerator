cfbe79b295bb13de95106eded471d163e1b858ae
Jaju67 <janskiba67@gmail.com>
2023-09-10 14:50:17 +0200

    added show function

--- interpreterapi.py
+++ interpreterapi.py
@@ -1,5 +1,7 @@
 from mathreader.api import *
 from sympy import preview
+from outwindow import outputWindow
+
 def imageRecognition(input_image_path, output_image_path):
   #INPUT image file path ex.: 'file.png' + define the output image path 
   #function interprets the image as a handwritten equation then generates LateX code of said equation
@@ -15,3 +17,8 @@ def imageRecognition(input_image_path, output_image_path):
   expression, img = recognizer.recognize()                                #-D 250 -> Setting dpi to 250 ; "-bg","Transparent" -> setting background to be transparent
   preview("$$" + expression + "$$", viewer = "file", filename = output_image_path, dvioptions=["-T", "tight", "-z", "0", "--truecolor", "-D 250","-bg","Transparent"])
   return expression     # the '$$' before and after expression are necessery
+
+
+def show(inputPath, windowNumber):
+  code = imageRecognition(inputPath, 'outputImage' + str(windowNumber))
+  copyWindow = outputWindow(code, 'outputImage' + str(windowNumber), 'Output ' + str(windowNumber))
